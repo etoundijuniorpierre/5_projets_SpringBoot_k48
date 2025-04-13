63c3fea387819bf21abd4f9f55c087b45dd5f0ea
ETOUNDI JUNIOR <162118158+etoundijuniorpierre@users.noreply.github.com>
2025-04-13 13:16:27 +0100

    Update todoListeBD.sql

--- projet_1_To-Do-List/database/todoListeBD.sql
+++ projet_1_To-Do-List/database/todoListeBD.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.2
 -- Dumped by pg_dump version 17.1
 
--- Started on 2025-04-05 14:31:35
+-- Started on 2025-04-11 14:41:10
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -24,7 +24,20 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 217 (class 1259 OID 57746)
+-- TOC entry 217 (class 1259 OID 65985)
+-- Name: hibernate_sequences; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.hibernate_sequences (
+    sequence_name character varying(255) NOT NULL,
+    next_val bigint
+);
+
+
+ALTER TABLE public.hibernate_sequences OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 65990)
 -- Name: todolist; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -40,41 +53,44 @@ CREATE TABLE public.todolist (
 ALTER TABLE public.todolist OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 57754)
--- Name: todolist_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.todolist_seq
-    START WITH 1
-    INCREMENT BY 50
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.todolist_seq OWNER TO postgres;
-
---
--- TOC entry 4843 (class 0 OID 57746)
+-- TOC entry 4848 (class 0 OID 65985)
 -- Dependencies: 217
--- Data for Name: todolist; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: hibernate_sequences; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.todolist (id, description, status, title) FROM stdin;
+COPY public.hibernate_sequences (sequence_name, next_val) FROM stdin;
+default	100
 \.
 
 
 --
--- TOC entry 4850 (class 0 OID 0)
+-- TOC entry 4849 (class 0 OID 65990)
 -- Dependencies: 218
--- Name: todolist_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Data for Name: todolist; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.todolist_seq', 1, false);
+COPY public.todolist (id, description, status, title) FROM stdin;
+1	todo list application backend api	encours	todo_1
+2	todo list application backend api phase_2	encours	todo_1
+3	todo list application backend api	encours	todo_1
+4	todo list application : installation des apllications de travail	termine	todo_2
+5	todo list application : installation des apllications de travail phase_2	termine	todo_2
+6	todo list application : interface frontend 	a_faire	todo_3
+7	todo list application : interface frontend phase_2	a_faire	todo_3
+\.
 
 
 --
--- TOC entry 4697 (class 2606 OID 57753)
+-- TOC entry 4700 (class 2606 OID 65989)
+-- Name: hibernate_sequences hibernate_sequences_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.hibernate_sequences
+    ADD CONSTRAINT hibernate_sequences_pkey PRIMARY KEY (sequence_name);
+
+
+--
+-- TOC entry 4702 (class 2606 OID 65997)
 -- Name: todolist todolist_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -82,7 +98,7 @@ ALTER TABLE ONLY public.todolist
     ADD CONSTRAINT todolist_pkey PRIMARY KEY (id);
 
 
--- Completed on 2025-04-05 14:31:35
+-- Completed on 2025-04-11 14:41:10
 
 --
 -- PostgreSQL database dump complete
